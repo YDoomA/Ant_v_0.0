5c6d3d93ab25ebfe7936da1d9296123095813977
Dooma <dooma_n@list.ru>
2018-12-22 17:15:24 +1000

    первая, адекватная рабочая версия муравья. Размер - 10х10. скорость - 1х

--- main.cpp
+++ main.cpp
@@ -3,11 +3,40 @@
 
 using namespace sf;
 using namespace std;
+
+void rect_move (int n,int &x, int &y, sf::RectangleShape rect, RenderWindow &app)
+{
+    switch (n)
+    {
+        case 1://up
+        y -= 1;
+        rect.setPosition(x*10,y*10);
+        break;
+
+        case 2://left
+        x -= 1;
+        rect.setPosition(x*10,y*10);
+        break;
+
+        case 3://down
+        y += 1;
+        rect.setPosition(x*10,y*10);
+        break;
+
+        case 4://right
+        x += 1;
+        rect.setPosition(x*10,y*10);
+        break;
+    }
+    app.draw(rect);
+}
+
+
 int main()
 {
     sf::RenderWindow app(sf::VideoMode(800, 600), "ANT v0.0");
 
-    int x=0,y=0;
+    int x=40,y=30, n=1;
 
     bool **arr = new bool* [81];
     for(int i=0; i< 81; i++)
@@ -20,7 +49,6 @@ int main()
     sf::RectangleShape _rectangle;
     _rectangle.setSize(sf::Vector2f(10,10));
     _rectangle.setFillColor(Color::Green);
-    //_rectangle.setPosition(x+400, y+300);
 
 
     while (app.isOpen())
@@ -37,20 +65,18 @@ int main()
                 }
         }
 
-        //app.clear();
-        if(arr[x][y] != true)
-        arr[x][y] = true;
-        else
-        arr[x][y] = false;
-        if(arr[x][y] == false) _rectangle.setFillColor(Color::Red);
-        else _rectangle.setFillColor(Color::Green);
-        _rectangle.setPosition(x*10,y*10);
-        if(x == 8){ x = 0; y+= 1;}
-        if(y == 6) {x=0;y=0;}
-        x+= 1;
-        app.draw(_rectangle);
+        if(arr[x][y] != true) {arr[x][y] = true; n++; if(n == 5) n=1;} // Если квадратик пустой или красный, то 1
+        else                  {arr[x][y] = false; n--; if(n == 0) n=4; }// Если квадратик 1, то 0
+
+        if(arr[x][y] == false) _rectangle.setFillColor(Color::Red); //Если 0, то красный
+        else                   _rectangle.setFillColor(Color::Green);// Если 1, то зелёный
+
+        //_rectangle.setPosition(x*10,y*10);
+        rect_move(n,x, y,_rectangle, app);
+
+        //app.draw(_rectangle);
         app.display();
-        sf::sleep(sf::milliseconds(10));
+        sf::sleep(sf::milliseconds(1));
     }
 
     return EXIT_SUCCESS;
